9286db5f6a208483f1830b13baaa3e59256bb986
scruffy <charlesmilam@gmail.com>
2017-07-24 22:29:07 -0500

    make use of serial port to receive data from uno

--- serial/serial.ino
+++ serial/serial.ino
@@ -4,6 +4,8 @@
 int ledPin = 13;
 
 void setup() {
+  // setup serial port
+  Serial.begin(9600);
   // make the pin act as Output, as we will write a value to it
   pinMode(13, OUTPUT);
 }
@@ -11,11 +13,15 @@ void setup() {
 void loop() {
   // write the value HIGH to the ledPin. switches the LED on
   digitalWrite(ledPin, HIGH);
+  // print on message
+  Serial.println("LED is ON");
   // pause for 3 seconds
   delay(3000);
   
   // write the value LOW, which turns the LED off
   digitalWrite(ledPin, LOW);
-  // pause for 3 seconds
+  // print off message
+  Serial.println("LED is OFF");
+  // pause for 3 s"conds
   delay(3000);
 }
